d2e4a8a206f7fbca4394746159b0c2560d2fd419
Nichlas Kvist Campos <nichlaskvistj@gmail.com>
2025-09-02 14:42:01 +0200

    posthog

---
+++ next.config.js
@@ -0,0 +1,18 @@
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/ingest/static/:path*',
+        destination: 'https://eu-assets.i.posthog.com/static/:path*',
+      },
+      {
+        source: '/ingest/:path*',
+        destination: 'https://eu.i.posthog.com/:path*',
+      },
+    ]
+  },
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
+}
+
+module.exports = nextConfig
